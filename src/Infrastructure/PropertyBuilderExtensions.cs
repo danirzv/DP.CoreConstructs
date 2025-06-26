27d380a7f30f5551bad3a473642c8c05dbfb2a5b
Dani rzv <danirzv@gmail.com>
2025-06-26 14:48:51 +0330

    Add PropertyBuilderExtensions

---
+++ src/Infrastructure/PropertyBuilderExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+using DP.CoreConstructs.Domain;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DP.CoreConstructs.Infrastructure;
+
+public static class PropertyBuilderExtensions
+{
+    public static JsonSerializerOptions JsonSerializerOptions = JsonSerializerOptions.Default;
+
+    public static PropertyBuilder<T> HasEnumerationConvertion<T>(this PropertyBuilder<T> builder) where T : Enumeration
+    {
+        return builder.HasConversion(
+            to => to.Id,
+            from => Enumeration.FromValue<T>(from));
+    }
+
+    public static PropertyBuilder<TProperty> HasJsonConversion<TProperty>(this PropertyBuilder<TProperty> builder)
+    {
+        return builder.HasConversion(
+            to => JsonSerializer.Serialize(to, typeof(TProperty), JsonSerializerOptions),
+            from => JsonSerializer.Deserialize<TProperty>(from, JsonSerializerOptions));
+    }
+
+    public static PropertyBuilder<TProperty> HasJsonConversion<TProperty>(this PropertyBuilder<TProperty> builder, TProperty defaultValue)
+    {
+        return builder.HasConversion(
+            to => JsonSerializer.Serialize(to, typeof(TProperty), JsonSerializerOptions),
+            from => string.IsNullOrEmpty(from)
+                ? defaultValue
+                : JsonSerializer.Deserialize<TProperty>(from, JsonSerializerOptions) ?? defaultValue);
+    }
+}
